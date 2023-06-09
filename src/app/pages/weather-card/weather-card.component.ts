72904f6d64dd86afa19dad9ec52df7ef7d5bc1e7
Tóth Tamás <toth.tamas19950628@gmail.com>
2023-06-09 18:57:13 +0200

    implement a method when search happens, gif update src and change the animation

--- src/app/pages/weather-card/weather-card.component.ts
+++ src/app/pages/weather-card/weather-card.component.ts
@@ -19,28 +19,54 @@ export class WeatherCardComponent implements OnInit {
   public weatherData: any;
   public srTime: any;
   public ssTime: any;
+  public searchTerm: any;
+  public weatherCondition: any;
+  public imageSrc: any;
 
   
 
   constructor(private weatherService: WeatherService) { }
-  @Input() public cityName: string = "Nádudvar"
+    public cityName: string = "Budapest"
  
   
   
   searchCountry(){
-    console.log(this.cityName)
-    return this.cityName
-   
+    
+      this.weatherService.getWeatherForCity(this.cityName).subscribe(
+        data => {
+          this.weatherData = data;
+          //console.log(this.weatherData);
+          this.weatherCondition = this.weatherData.weather[0].main
+          this.srTime = new Date(this.weatherData.sys.sunrise*1000).getHours()+":"+new Date(this.weatherData.sys.sunrise*1000).getMinutes()
+          this.ssTime = new Date(this.weatherData.sys.sunset*1000).getHours()+":"+new Date(this.weatherData.sys.sunset*1000).getMinutes()
+          console.log(this.weatherCondition)
+          this.setImage()
+      })
   }
+
+  setImage() {
+    if(this.weatherCondition === 'Clear') {
+      this.imageSrc = 'assets/icons8-sun.gif'
+    } else if(this.weatherCondition === 'Rain') {
+      this.imageSrc = 'assets/icons8-rain.gif'
+     } else if(this.weatherCondition === 'Clouds') {
+      this.imageSrc = 'assets/icons8-partly-cloudy-day.gif'
+    } else if (this.weatherCondition === 'Snow'){
+      this.imageSrc = 'assets/icons8-snow.gif'
+    }
+  } 
   
 
   ngOnInit() {
 
-   this.weatherService.getWeatherForCity(this.cityName).subscribe(data => {this.weatherData = data; console.log(this.weatherData)
-  
-    this.srTime = new Date(this.weatherData.sys.sunrise*1000).getHours()+":"+new Date(this.weatherData.sys.sunrise*1000).getMinutes()
-    this.ssTime = new Date(this.weatherData.sys.sunset*1000).getHours()+":"+new Date(this.weatherData.sys.sunset*1000).getMinutes()
-  })
+   this.weatherService.getWeatherForCity(this.cityName).subscribe(
+    data => {
+      this.weatherData = data;
+      this.weatherCondition = this.weatherData.weather[0].main
+      
+      //console.log(this.weatherData);
+      this.setImage()
+   })
   }
 
  
